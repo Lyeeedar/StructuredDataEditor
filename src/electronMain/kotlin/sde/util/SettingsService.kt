810ca7ab1f1a9d49e0deb2feb0fd6b69baafaf3b
Philip Liddell <lyeeedar@gmail.com>
2020-07-19 17:21:10 +0100

    Add support for saving and loading settings

--- src/electronMain/kotlin/sde/util/SettingsService.kt
+++ src/electronMain/kotlin/sde/util/SettingsService.kt
@@ -1,11 +1,35 @@
 package sde.util
 
+import kotlinx.serialization.json.Json
+import node.fs.fs
+
 actual class SettingsService : ISettingsService {
+    private var settings: Settings? = null
+        get() {
+            if (field == null) {
+                field = try {
+                    val json = fs.readFileStringSync(settingsFile, "utf8")
+                    Json.parse(Settings.serializer(), json)
+                } catch(ex: Throwable) {
+                    Settings()
+                }
+            }
+
+            return field
+        }
+
+    private val settingsFile = "sdeSettings.data"
+
     override suspend fun loadSettings(): Settings {
-        return Settings()
+        return settings!!
     }
 
     override suspend fun saveSettings(settings: Settings): Boolean {
+        val json = Json.stringify(Settings.serializer(), settings)
+        fs.writeFileSync(settingsFile, json)
+
+        this.settings = settings
+
         return true
     }
 }
