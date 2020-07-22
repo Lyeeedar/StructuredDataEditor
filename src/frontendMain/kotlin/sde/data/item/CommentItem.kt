94ce9c693f8b3bba404f89b74335cefdc6a1840b
lyeeedar <lyeeedar@gmail.com>
2020-07-22 14:07:06 +0100

    Improve theme more

--- src/frontendMain/kotlin/sde/data/item/CommentItem.kt
+++ src/frontendMain/kotlin/sde/data/item/CommentItem.kt
@@ -1,11 +1,11 @@
 package sde.data.item
 
-import pl.treksoft.kvision.core.Component
-import pl.treksoft.kvision.core.CssSize
-import pl.treksoft.kvision.core.UNIT
+import pl.treksoft.kvision.core.*
+import pl.treksoft.kvision.html.Align
 import pl.treksoft.kvision.html.Div
 import pl.treksoft.kvision.html.Span
 import pl.treksoft.kvision.panel.DockPanel
+import pl.treksoft.kvision.panel.GridPanel
 import pl.treksoft.kvision.panel.Side
 import sde.data.DataDocument
 import sde.data.definition.CommentDefinition
@@ -25,20 +25,10 @@ class CommentItem(document: DataDocument, initialValue: String = "") : AbstractD
 	{
 		val depth = depth * 14 + 14
 
-		return DockPanel {
+		return GridPanel(templateColumns = "1fr auto 1fr") {
 			marginLeft = CssSize(depth, UNIT.px)
 
-			add(Div {
-				height = CssSize(1, UNIT.px)
-				width = CssSize(100, UNIT.perc)
-			}, Side.LEFT)
-
-			add(Div {
-				height = CssSize(1, UNIT.px)
-				width = CssSize(100, UNIT.perc)
-			}, Side.RIGHT)
-
-			add(Span(value))
+			add(Span(value), 2)
 		}
 	}
 
