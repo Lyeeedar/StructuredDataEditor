190fc2ce04dd4f36f1184b545689938d8bd81472
Philip Liddell <lyeeedar@gmail.com>
2020-08-30 19:11:01 +0100

    Improve graph look. Support comments

--- src/frontendMain/kotlin/sde/ui/graph/GraphNodeDataItem.kt
+++ src/frontendMain/kotlin/sde/ui/graph/GraphNodeDataItem.kt
@@ -2,6 +2,7 @@ package sde.ui.graph
 
 import org.w3c.dom.CanvasRenderingContext2D
 import pl.treksoft.kvision.html.Align
+import sde.data.item.CommentItem
 import sde.data.item.DataItem
 import sde.ui.*
 import sde.utils.removeTags
@@ -13,6 +14,10 @@ abstract class AbstractGraphNodeDataItem(val item: DataItem, val node: GraphNode
 
     companion object {
         fun create(item: DataItem, node: GraphNode): AbstractGraphNodeDataItem {
+            if (item is CommentItem) {
+                return CommentGraphNodeDataItem(item, node)
+            }
+
             return PreviewGraphNodeDataItem(item, node)
         }
     }
@@ -25,7 +30,7 @@ class PreviewGraphNodeDataItem(item: DataItem, node: GraphNode) : AbstractGraphN
     override fun getWidth(context2D: CanvasRenderingContext2D): Double {
         val margin = margin * node.graph.scale
         val nameBounds = context2D.measureText((fontSize * node.graph.scale).toInt(), item.name)
-        val descBounds = context2D.measureText((fontSize * node.graph.scale).toInt(), item.description.removeTags())
+        val descBounds = context2D.measureText((fontSize * node.graph.scale).toInt(), item.description.removeTags().take(20))
         return nameBounds.width + descBounds.width + margin * 3
     }
 
@@ -48,6 +53,39 @@ class PreviewGraphNodeDataItem(item: DataItem, node: GraphNode) : AbstractGraphN
         textBounds.height -= margin * 2
 
         context2D.drawText(fontSize, item.def.textColour, item.name, textBounds, Align.LEFT)
-        context2D.drawText(fontSize, "white", item.description.removeTags(), textBounds, Align.RIGHT)
+        context2D.drawText(fontSize, "white", item.description.removeTags().take(20), textBounds, Align.RIGHT)
     }
+}
+
+class CommentGraphNodeDataItem(item: CommentItem, node: GraphNode) : AbstractGraphNodeDataItem(item, node) {
+    private val fontSize = 10
+    private val margin = 2
+
+    override fun getWidth(context2D: CanvasRenderingContext2D): Double {
+        val item = item as CommentItem
+        val margin = margin * node.graph.scale
+        val bounds = context2D.measureText((fontSize * node.graph.scale).toInt(), item.value)
+        return bounds.width + margin * 2
+    }
+
+    override fun getHeight(context2D: CanvasRenderingContext2D): Double {
+        val margin = margin * node.graph.scale
+        return fontSize * node.graph.scale + margin * 2
+    }
+
+    override fun draw(context2D: CanvasRenderingContext2D, bounds: BoundingBox) {
+        val item = item as CommentItem
+
+        val fontSize = (fontSize * node.graph.scale).toInt()
+        val margin = margin * node.graph.scale
+
+        val textBounds = BoundingBox(bounds)
+        textBounds.x += margin
+        textBounds.y += margin
+        textBounds.width -= margin * 2
+        textBounds.height -= margin * 2
+
+        context2D.drawText(fontSize, "grey", item.value, textBounds, Align.CENTER)
+    }
+
 }
