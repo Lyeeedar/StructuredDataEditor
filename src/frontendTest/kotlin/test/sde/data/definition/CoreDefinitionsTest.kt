fc0b9c7fc3764a42f25def26990810e06cbcffd1
lyeeedar <lyeeedar@gmail.com>
2020-08-31 09:44:14 +0100

    Add test to check sandbox loads

--- src/frontendTest/kotlin/test/sde/data/definition/CoreDefinitionsTest.kt
+++ src/frontendTest/kotlin/test/sde/data/definition/CoreDefinitionsTest.kt
@@ -1,7 +1,11 @@
 package test.sde.data.definition
 
 import sde.data.DataDocument
+import sde.data.Project
 import sde.data.definition.CoreDefinitions
+import sde.data.definition.DefinitionMap
+import sde.data.definition.StructDefinition
+import sde.data.item.StructItem
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -26,4 +30,41 @@ class CoreDefinitionsTest
         val item = CoreDefinitions.rootDef.createItem(DataDocument(""))
         assertNotNull(item)
     }
+
+	@Test
+	fun sandbox() {
+		val xml = pl.treksoft.kvision.require("test/TestProject/Definitions/Sandbox.xmldef") as String
+		val auxXml = pl.treksoft.kvision.require("test/TestProject/Definitions/AuxFiles.xmldef") as String
+
+		val maps = HashMap<String, DefinitionMap>()
+		maps["Sandbox"] = Project.parseDefinitionsFile(xml, "Sandbox")
+		maps["AuxFiles"] = Project.parseDefinitionsFile(auxXml, "AuxFiles")
+
+		val global = DefinitionMap()
+		for (defMap in maps.values)
+		{
+			for (def in defMap.values)
+			{
+				if (def.isGlobal || !def.isDef) {
+					global[def.name] = def
+				}
+			}
+		}
+
+		for (defMap in maps.values)
+		{
+			for (def in defMap.values)
+			{
+				def.resolve(global, maps)
+			}
+		}
+
+		val def = maps["Sandbox"]!!["Sandbox"]
+		assertNotNull(def)
+		assertTrue(def is StructDefinition)
+
+		val doc = DataDocument("")
+		val item = def.createItem(doc)
+		assertTrue(item is StructItem)
+	}
 }
